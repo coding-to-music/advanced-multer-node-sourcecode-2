89aaaaa68408805bd7a08c866b0a968130319124
coding-to-music <connors.tom@gmail.com>
2021-11-11 07:12:27 +0000

    first commit

---
+++ helpers/CloudStorage.js
@@ -0,0 +1,15 @@
+// import axios from 'axios';
+
+
+// Simple PUT request with a JSON body using axios
+// const element = document.querySelector('#put-request .date-updated');
+// const article = { title: 'Axios PUT Request Example' };
+// axios.put('https://reqres.in/api/articles/1', article)
+//     .then(response => element.innerHTML = response.data.updatedAt);
+
+
+// Simple PUT request with a JSON body using axios
+// const element = 'foo';
+// const article = { title: 'Axios PUT Request Example' };
+// axios.put('https://reqres.in/api/articles/1', article)
+//     .then(response => element.innerHTML = response.data.updatedAt);    
